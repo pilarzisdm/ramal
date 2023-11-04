5b39edac33820c0e77cfb01f32ce77caa64d44e7
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-04 12:52:54 +0000

    y

--- ramal.py
+++ ramal.py
@@ -39,7 +39,10 @@ if len(commodities) > 0:
         for commodity in commodities:
             # Calculate forecasts for the selected number of days into the future
             last_date = forecast_data.index[-1]
-            forecast_dates = [last_date + pd.DateOffset(days=day) for day in range(1, forecasting_days + 1)]
+            forecast_dates = []
+            for day in range(1, forecasting_days + 1):
+                next_date = last_date + pd.DateOffset(days=day)
+                forecast_dates.append(next_date)
             forecast_values = [None] * forecasting_days
             forecast_data = forecast_data.append(pd.DataFrame({commodity: forecast_values}, index=forecast_dates))
 
