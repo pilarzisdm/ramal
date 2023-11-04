574c5a0da2c9ed9ff441564b53529c4e497f0dfc
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-04 13:11:45 +0000

    y

--- ramal.py
+++ ramal.py
@@ -21,9 +21,10 @@ data = load_data()
 
 # Filter data based on selected commodities
 if len(commodities) > 0:
-    st.subheader("Harga Komoditas")
     selected_data = data[['Tanggal'] + commodities]
     selected_data = selected_data.sort_values(by='Tanggal', ascending=False)
+
+    st.subheader("Harga Komoditas")
     selected_data['Tanggal'] = selected_data['Tanggal'].dt.date  # Extract date portion
     st.write(selected_data.set_index('Tanggal'))
 
@@ -36,27 +37,18 @@ if len(commodities) > 0:
         forecast_data = selected_data.copy()
 
         for commodity in commodities:
-            # Create a separate DataFrame for each commodity
-            forecast_data_commodity = forecast_data[['Tanggal', commodity]].copy()
-
-            if forecast_data_commodity.empty:
-                continue
-
             # Calculate the Simple Moving Average (SMA) for the commodity
-            forecast_data_commodity[commodity + '_SMA'] = forecast_data_commodity[commodity].rolling(window=7).mean()
+            forecast_data[commodity + '_SMA'] = forecast_data[commodity].rolling(window=7).mean()
 
             # Use the SMA to forecast future values
-            last_date = forecast_data_commodity['Tanggal'].max()
+            last_date = forecast_data['Tanggal'].max()
             forecast_dates = pd.date_range(start=last_date + pd.DateOffset(1), periods=forecasting_days)
-            forecast_values = [None] * forecasting_days
+            forecast_values = [forecast_data[commodity + '_SMA'].iloc[-1]] * forecasting_days
             forecast_df = pd.DataFrame({commodity: forecast_values}, index=forecast_dates)
-
-            # Merge the forecasted data for each commodity into the final forecast data
-            forecast_data_commodity = forecast_data_commodity.merge(forecast_df, left_index=True, right_index=True, how='outer')
-            forecast_data = forecast_data.merge(forecast_data_commodity[['Tanggal', commodity, commodity + '_SMA']], on='Tanggal', how='outer')
+            forecast_data = forecast_data.append(forecast_df)
 
         # Display the forecasted data
-        st.write(forecast_data.tail(forecasting_days))
+        st.write(forecast_data.tail(forecasting_days)[commodities])
 
 else:
     st.warning("Silakan pilih satu atau lebih komoditas.")
