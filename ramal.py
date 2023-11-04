ec59b64ff411f1863b9d74b310c733f463950eaa
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-04 13:09:04 +0000

    y

--- ramal.py
+++ ramal.py
@@ -21,10 +21,9 @@ data = load_data()
 
 # Filter data based on selected commodities
 if len(commodities) > 0:
+    st.subheader("Harga Komoditas")
     selected_data = data[['Tanggal'] + commodities]
     selected_data = selected_data.sort_values(by='Tanggal', ascending=False)
-
-    st.subheader("Harga Komoditas")
     selected_data['Tanggal'] = selected_data['Tanggal'].dt.date  # Extract date portion
     st.write(selected_data.set_index('Tanggal'))
 
@@ -43,14 +42,15 @@ if len(commodities) > 0:
             # Calculate the Simple Moving Average (SMA) for the commodity
             forecast_data_commodity[commodity + '_SMA'] = forecast_data_commodity[commodity].rolling(window=7).mean()
 
-            # Use the SMA to forecast future values for each commodity
+            # Use the SMA to forecast future values
             last_date = forecast_data_commodity['Tanggal'].max()
             forecast_dates = pd.date_range(start=last_date + pd.DateOffset(1), periods=forecasting_days)
             forecast_values = [None] * forecasting_days
             forecast_df = pd.DataFrame({commodity: forecast_values}, index=forecast_dates)
 
             # Merge the forecasted data for each commodity into the final forecast data
-            forecast_data = forecast_data.merge(forecast_df, left_index=True, right_index=True, how='outer')
+            forecast_data_commodity = forecast_data_commodity.merge(forecast_df, left_index=True, right_index=True, how='outer')
+            forecast_data = forecast_data.merge(forecast_data_commodity[['Tanggal', commodity, commodity + '_SMA']], on='Tanggal', how='outer')
 
         # Display the forecasted data
         st.write(forecast_data.tail(forecasting_days))
