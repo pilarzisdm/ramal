04af911a6dd9d05ff55871b64679eaedd54d472a
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-04 00:22:38 +0000

    y

--- ramal.py
+++ ramal.py
@@ -1,91 +1,79 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from statsmodels.tsa.arima_model import ARIMA
-from statsmodels.tsa.holtwinters import ExponentialSmoothing
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-
-# Load historical data
+# Load the CSV data
 @st.cache_data
 def load_data():
     data = pd.read_csv("harga_real.csv")
+    data['Tanggal'] = pd.to_datetime(data['Tanggal'])  # Parse the date column as datetime
     return data
 
+# Sidebar: Select commodities
+st.sidebar.title("Pilih Komoditas")
+commodities = st.sidebar.multiselect("Pilih satu atau lebih komoditas", ["Beras", "Daging Ayam", "Telur Ayam", "Cabai Merah", "Cabai Rawit"])
+
+# Main content
+st.title("Peramalan Harga Komoditas Harian")
+
+# Load data
 data = load_data()
 
-# User selects a commodity to forecast
-commodity_choice = st.selectbox("Select Commodity", data.columns[1:])  # Assuming the first column is "Date"
+# Filter data based on selected commodities
+if len(commodities) > 0:
+    selected_data = data[['Tanggal'] + commodities]
+    selected_data = selected_data.sort_values(by='Tanggal', ascending=False)
 
-# Split data into training and testing sets
-X = data.drop(columns=["Tanggal", commodity_choice])
-y = data[commodity_choice]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    st.subheader("Harga Komoditas")
+    selected_data['Tanggal'] = selected_data['Tanggal'].dt.date  # Extract date portion
+    st.write(selected_data.set_index('Tanggal'))
 
-# User selects forecasting model
-model_choice = st.selectbox("Select Forecasting Model", ["Random Forest", "ARIMA", "Exponential Smoothing", "Moving Average"])
+    # Add select box for granularity just for the plot
+    granularity = st.selectbox("Pilih Granularitas Tanggal untuk Grafik", ["Harian", "Mingguan", "Bulanan"])
 
-# User specifies the number of periods to forecast
-forecast_periods = st.text_input("Enter the number of periods to forecast:", 1)  # Default to 1 period
+    # Determine granularity interval
+    if granularity == "Mingguan":
+        granularity_interval = 'W'
+    elif granularity == "Bulanan":
+        granularity_interval = 'M'
+    else:
+        granularity_interval = 'D'
 
-if model_choice == "Random Forest":
-    # Train a Random Forest model
-    model = RandomForestRegressor()
-    model.fit(X_train, y_train)
-    # Make predictions
-    predictions = model.predict(X_test)
+    # Convert the index to DatetimeIndex
+    selected_data['Tanggal'] = pd.to_datetime(selected_data['Tanggal'])
+    selected_data.set_index('Tanggal', inplace=True)
 
-elif model_choice == "ARIMA":
-    # Train an ARIMA model
-    model = ARIMA(y_train, order=(5, 1, 0))
-    model = model.fit(disp=0)
-    # Make predictions
-    predictions = model.forecast(steps=int(forecast_periods), alpha=0.05)
+    # Resample data for the selected granularity
+    resampled_data = selected_data.resample(granularity_interval).last()
 
-elif model_choice == "Exponential Smoothing":
-    # Train an Exponential Smoothing model
-    model = ExponentialSmoothing(y_train, trend='add', seasonal='add', seasonal_periods=12)
-    model = model.fit()
-    # Make predictions
-    predictions = model.forecast(int(forecast_periods))
+    # Plot selected commodities with the selected granularity
+    st.subheader("Grafik Harga Komoditas")
+    fig, ax = plt.subplots(figsize=(10, 5))
+
+    for commodity in commodities:
+        ax.plot(resampled_data.index, resampled_data[commodity], label=commodity)
+
+    ax.set_xlabel("Tanggal")
+    ax.set_ylabel("Harga")
+    ax.set_title("Harga Komoditas Antar Waktu")
+    ax.legend()
+
+    st.pyplot(fig)
+
+    # Perform multi-day-ahead forecasting for selected commodities
+    st.subheader("Peramalan Harga Komoditas untuk Hari Berikutnya")
+
+    forecasting_period = st.number_input("Masukkan jumlah hari untuk peramalan:", min_value=1, step=1)
+
+    if st.button("Forecast"):
+        forecast_data = resampled_data.copy()
+
+        for commodity in commodities:
+            # Calculate the multi-day-ahead forecast using a rolling mean
+            forecast_data[commodity + ' (Forecast)'] = forecast_data[commodity].rolling(window=forecasting_period).mean()
+
+        # Display the forecasted data
+        st.write(forecast_data.tail(forecasting_period))
 
 else:
-    # Use Moving Average for forecasting
-    predictions = y_train.rolling(window=12).mean().iloc[-1]  # Use a simple 12-month moving average for forecasting
-
-# Model evaluation
-mae = mean_absolute_error(y_test, predictions)
-mse = mean_squared_error(y_test, predictions)
-rmse = np.sqrt(mse)
-
-st.title("Market Prices Forecasting")
-
-st.write("## Historical Data")
-st.write(data)
-
-st.write(f"## Forecasting for {commodity_choice}")
-st.write(f"### Model: {model_choice}")
-st.write(f"### Forecasting Periods: {forecast_periods} periods")
-st.write(f"#### Model Evaluation")
-st.write(f"Mean Absolute Error (MAE): {mae}")
-st.write(f"Mean Squared Error (MSE): {mse}")
-st.write(f"Root Mean Squared Error (RMSE): {rmse}")
-
-st.write("#### Model Predictions")
-st.write(predictions)
-
-st.write("#### Actual vs. Predicted Data")
-comparison_data = pd.DataFrame({'Actual': y_test, 'Predicted': predictions})
-st.write(comparison_data)
-
-st.line_chart(predictions)
-
-# Optionally, you can plot historical vs. predicted data
-plt.figure(figsize=(10, 6))
-plt.plot(y_test, label='Historical Data')
-plt.plot(predictions, label='Predictions')
-plt.legend()
-st.pyplot(plt)
+    st.warning("Silakan pilih satu atau lebih komoditas.")
